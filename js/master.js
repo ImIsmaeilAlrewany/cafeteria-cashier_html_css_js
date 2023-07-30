e0df02184808dcbac5f08e28bf50ac1d7a711246
imIsmailAlrewany <ismailalrewany332@gmail.com>
2023-07-30 22:31:31 +0200

    cafeteria cashier

--- js/master.js
+++ js/master.js
@@ -301,18 +301,23 @@ if (profileName) profileName.value = profileData.name;
 if (profilePhone) profilePhone.value = profileData.phone;
 if (profilePassword) profilePassword.value = '*'.repeat(profileData.password.length);
 
+// get the clientIndex (cashier) after comparing it with local storage
+const getClientIndex = (arr, client) => {
+  let index;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].name === client.name && arr[i].phone === client.phone && arr[i].password === client.password) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+};
+
 // submit new data will be after clicking on edit profile button
 if (profile) profile.addEventListener('submit', (e) => {
   const clients = JSON.parse(localStorage.getItem('clients')) || [];
   let isFormValid = false;
-  let clientIndex;
-
-  for (let i = 0; i < clients.length; i++) {
-    if (clients[i].name === profileData.name && clients[i].phone === profileData.phone && clients[i].password === profileData.password) {
-      clientIndex = i;
-      break;
-    }
-  }
+  let clientIndex = getClientIndex(clients, profileData);
 
   const data = collectData([profileName, profilePhone, profileNewPassword]);
   const oldPassword = profilePassword.value.trim();
@@ -398,6 +403,25 @@ if (profile) profile.addEventListener('submit', (e) => {
     sessionStorage.setItem('onlineClient', JSON.stringify(clients[clientIndex]));
     profile.submit();
   }
+});
+
+// delete cashier account
+const deleteProfile = document.getElementById('profile-cashier-delete');
+
+if (deleteProfile) deleteProfile.addEventListener('click', () => {
+  const clients = JSON.parse(localStorage.getItem('clients'));
+  let clientIndex = getClientIndex(clients, profileData);
+
+  // set data in session storage
+  sessionStorage.removeItem('onlineClient');
+  sessionStorage.setItem('isLoggedIn', false);
+
+  // set data in local storage
+  clients.splice(clientIndex, 1);
+  localStorage.setItem('clients', JSON.stringify(clients));
+
+  // redirect to login page
+  location.href = '../login.html';
 });
 
 //===============================================//
