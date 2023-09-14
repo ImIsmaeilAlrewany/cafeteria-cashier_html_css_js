439732973bb5e2a4059e877e181b34309e009b0f
imIsmailAlrewany <ismailalrewany332@gmail.com>
2023-09-14 16:08:07 +0200

    cafeteria cashier

--- js/master.js
+++ js/master.js
@@ -1285,6 +1285,9 @@ if (orderTable) orderTable.addEventListener('click', () => {
     localStorage.setItem('cafeteria-tables', JSON.stringify(tables));
 
     // this is chat GPT idea
+    // in this solution a problem which is when I open website locally won't work
+    // so I always need a host to work properly lik liveServer or gitHub host
+    // Error(Blocked a frame with origin "null" from accessing a cross-origin frame)
     var iframe = document.createElement('iframe');
     iframe.style.display = 'none';
     iframe.src = 'print.html';
@@ -1357,6 +1360,50 @@ if (payOrder) payOrder.addEventListener('click', () => {
 });
 
 //================================================//
+
+// work on notification system for categories items
+// if an item is about to run out will show a notification message every 10 seconds
+function displayNotification(item) {
+  const div = document.createElement('div');
+  const p = document.createElement('p');
+  div.className = 'notification position-absolute border-0 rounded mx-3 shadow';
+  p.className = 'p-3 text-center m-0';
+  p.textContent = `${item} على وشك النفاذ`;
+  div.appendChild(p);
+  document.body.appendChild(div);
+
+  setTimeout(() => {
+    div.remove();
+  }, 3000); // remove after 3 seconds
+}
+
+const notificationHandler = (array) => {
+  const menu = JSON.parse(localStorage.getItem('menu-categories'));
+
+  // find all items quantity below 10 and save them in an Array
+  menu.forEach(category => {
+    category.content.forEach(item => {
+      if (item.quantity <= 10) {
+        array.push(item.name);
+      }
+    });
+  });
+};
+
+function notification() {
+  let itemsBelow = [];
+  let delay = 0;
+  notificationHandler(itemsBelow);
+
+  // this AI solution still the output is reversed
+  for (let i = 0; i < itemsBelow.length; i++) {
+    delay += 3000;
+    setTimeout(() => displayNotification(itemsBelow[i]), delay + (i * 3000));
+  }
+
+  setTimeout(notification, delay + 10000);
+}
+notification();
 
 
 
