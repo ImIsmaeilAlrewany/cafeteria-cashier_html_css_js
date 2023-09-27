c1d8f4df9a227dd9341a88254bbde8eb88e01639
imIsmailAlrewany <ismailalrewany332@gmail.com>
2023-09-27 04:36:54 +0200

    cafeteria cashier

--- js/layouts/includeHTML.js
+++ js/layouts/includeHTML.js
@@ -1,5 +1,11 @@
-const includeHTML = async (url, container) => {
+const includeHTML = async (url, container, script = false) => {
   const response = await fetch(url);
   const htmlContent = await response.text();
   document.querySelector(container).innerHTML = htmlContent;
+
+  // Evaluate the scripts in the implemented file 
+  if (script) {
+    const scripts = document.querySelector(container).querySelectorAll("script");
+    for (const script of scripts) { eval(script.textContent); };
+  }
 };
--- js/report.js
+++ js/report.js
@@ -1,6 +1,24 @@
 import selectFromMenu from './layouts/selectFromMenu.js';
 
+const reference = [
+  { name: 'general', path: '../layouts/report-general.html' },
+  { name: 'done', path: '../layouts/report-done.html' },
+  { name: 'canceled', path: '../layouts/report-canceled.html' },
+  { name: 'runout', path: '../layouts/report-runout.html' },
+  { name: 'cashier', path: '../layouts/report-cashier.html' },
+];
+
+// when loading the page first includes general file
+includeHTML(reference[0].path, '.option-details', true);
+
 // work on report page and starts with select from menu options
 const optionsList = document.querySelectorAll('.report .options li');
 
-if (optionsList) selectFromMenu(optionsList, () => { });
+if (optionsList) selectFromMenu(optionsList, (index) => {
+  for (let i = 0; i < reference.length; i++) {
+    if (optionsList[index].dataset.option === reference[i].name) {
+      includeHTML(reference[i].path, '.option-details', true);
+    }
+  }
+});
+
